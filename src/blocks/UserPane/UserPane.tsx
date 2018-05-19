c7e1d0a48da3f6af06c0534d3cfe46eac461db56
esow <simmerkaer@gmail.com>
2018-05-19 10:05:06 +0200

    Added circles for showing PlayedGames and WonGames

--- src/blocks/UserPane/UserPane.tsx
+++ src/blocks/UserPane/UserPane.tsx
@@ -19,11 +19,26 @@ export default class UserPane extends React.Component<UserPaneProps, any> {
 	};
 
 	render() {
-		const { platform, username, top5finishes, top3finishes, top1finishes } = this.props;
+		const { username, top5finishes, top3finishes, top1finishes, gamesPlayed, gamesWon } = this.props;
 		return (
 			<div className="summoner-pane">
-				{`platform: ${platform} username: ${username}`}
-				<BarChart top5={top5finishes} top3={top3finishes} top1={top1finishes} />
+				<div className="column user-input">
+					<div className="header">{username}</div>
+				</div>
+				<div className="column statistics">
+					<div className="header">Statistics</div>
+					<div className="circles">
+						<div className="circle played">
+							<div className="text">played</div>
+							{gamesPlayed}
+						</div>
+						<div className="circle won">
+							<div className="text">won</div>
+							{gamesWon}
+						</div>
+					</div>
+					<BarChart top5={top5finishes} top3={top3finishes} top1={top1finishes} />
+				</div>
 			</div>
 		);
 	}
