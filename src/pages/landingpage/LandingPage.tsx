f16de3bc640691f5adfdf9afbb06b3963c566e32
MPKohl <martin.pagh.kohl@gmail.com>
2018-09-22 23:17:01 +0200

    added game selection to landing page

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import "./LandingPage.css";
+import GameSelector from "../../components/GameSelector/GameSelector";
 
 export interface LandingPageProps {
 }
 
 export default class LandingPage extends React.Component<LandingPageProps, any> {
+
 	render() {
+		const listOfGames = [
+			{ imageSrc: "/GameSelectorFortnite-640x360.png", title: "Fortnite - Battle Royale", playerCount: 4274 },
+			{ imageSrc: "/GameSelectorLeagueOfLegends-640x360.png", title: "League of Legends", playerCount: 1 }];
 		return (
 			<div className="landing-page">
 				<div className="page-content">
 					<p className="buddy" >Buddy<span>GG</span></p>
 					<p>FORTNITE</p>
+					<GameSelector listOfGames={listOfGames} />
 				</div>
 			</div>
 		);
--- stories/dimmablecard.stories.tsx
+++ stories/dimmablecard.stories.tsx
@@ -5,9 +5,5 @@ import DimmableCard from "../src/components/DimmableCard/DimmableCard";
 
 storiesOf("DimmableCard", module)
 	.add("Fortnite as example", () =>
-		<DimmableCard
-			imageSrc="/GameSelectorFortnite-640x360.png"
-			title="Fortnite - Battle Royale"
-			playerCount={2}
-		/>
+		<DimmableCard imageSrc="/GameSelectorFortnite-640x360.png" title="Fortnite - Battle Royale" playerCount={2} />
 	);
