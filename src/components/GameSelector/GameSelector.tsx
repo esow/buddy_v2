deda2cbf94722c14baaadc402806ef31f1b8c4e6
ecly <ecly@mailbox.org>
2018-09-22 23:58:43 +0200

    integrate footer with landing page and fix missing key in map

--- src/components/GameSelector/GameSelector.tsx
+++ src/components/GameSelector/GameSelector.tsx
@@ -3,34 +3,35 @@ import { Card } from "semantic-ui-react";
 import DimmableCard from "../DimmableCard/DimmableCard";
 
 export interface GameSelectorProps {
-	listOfGames: {
-		imageSrc: string;
-		title: string;
-		playerCount: Number;
-	}[];
+    listOfGames: {
+        imageSrc: string;
+        title: string;
+        playerCount: Number;
+    }[];
 
 }
 
 export default class GameSelector extends React.Component<GameSelectorProps, any> {
 
-	render() {
+    render() {
 
-		return (
-			<Card.Group centered>
-				{
-					// tslint:disable-next-line:typedef
-					this.props.listOfGames.map(function (elem) {
-						return (
-							// tslint:disable-next-line:jsx-key
-							<DimmableCard
-								imageSrc={elem.imageSrc}
-								title={elem.title}
-								playerCount={elem.playerCount}
-							/>
-						);
-					})
-				}
-			</Card.Group>);
+        return (
+            <Card.Group centered>
+            {
+                // tslint:disable-next-line:typedef
+                this.props.listOfGames.map((elem, _) => {
+                    // tslint:disable-next-line:jsx-key
+                    return (
+                        <DimmableCard
+                            key={elem.title}
+                            imageSrc={elem.imageSrc}
+                            title={elem.title}
+                            playerCount={elem.playerCount}
+                        />
+                    );
+                })
+            }
+            </Card.Group>);
 
-	}
+    }
 }
