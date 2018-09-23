076339256014c0476a44dc6ddd081b6edeea0047
MPKohl <martin.pagh.kohl@gmail.com>
2018-09-23 16:26:24 +0200

    add functionality to go from landing page to game pages

--- stories/dimmablecard.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import "semantic-ui-css/semantic.min.css";
-import { storiesOf } from "@storybook/react";
-import DimmableCard from "../src/components/DimmableCard/DimmableCard";
-
-storiesOf("DimmableCard", module)
-	.add("Fortnite as example", () =>
-		<DimmableCard imageSrc="/GameSelectorFortnite-640x360.png" title="Fortnite - Battle Royale" playerCount={2} />
-	);
