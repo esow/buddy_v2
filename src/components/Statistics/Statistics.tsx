d6086e747adc377bfd1f0af2abc65c5a5082798b
MPKohl <martin.pagh.kohl@gmail.com>
2018-09-23 17:17:20 +0200

    add color to stat icons

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -14,27 +14,27 @@ const Statistics: React.SFC<StatisticProps> = (props) => {
 		<Statistic.Group widths="one" size={"tiny"}>
 			<Statistic>
 				<Statistic.Value>{props.wins}</Statistic.Value>
-				<Statistic.Label><Icon name="winner" />Wins</Statistic.Label>
+				<Statistic.Label><Icon name="winner" color="yellow" />Wins</Statistic.Label>
 			</Statistic>
 
 			<Statistic>
 				<Statistic.Value>{props.top5}</Statistic.Value>
-				<Statistic.Label><Icon name="angle double up" />Top5</Statistic.Label>
+				<Statistic.Label><Icon name="angle double up" color="green" />Top5</Statistic.Label>
 			</Statistic>
 
 			<Statistic>
 				<Statistic.Value>{props.top12}</Statistic.Value>
-				<Statistic.Label><Icon name="angle up" />Top12</Statistic.Label>
+				<Statistic.Label><Icon name="angle up" color="green" />Top12</Statistic.Label>
 			</Statistic>
 
 			<Statistic>
 				<Statistic.Value>{props.played}</Statistic.Value>
-				<Statistic.Label><Icon name="game" />Played</Statistic.Label>
+				<Statistic.Label><Icon name="game" color="violet" />Played</Statistic.Label>
 			</Statistic>
 
 			<Statistic>
 				<Statistic.Value>{(props.kdratio).toPrecision(3)}</Statistic.Value>
-				<Statistic.Label><Icon name="bullseye" />K/D-Ratio</Statistic.Label>
+				<Statistic.Label><Icon name="bullseye" color="red" />K/D-Ratio</Statistic.Label>
 			</Statistic>
 		</Statistic.Group>
 	);
