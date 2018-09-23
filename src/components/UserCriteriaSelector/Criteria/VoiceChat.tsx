93ee4a691bb74ca74e6c5ffed14a24daa3b0a1b5
Hans <hanspagh@gmail.com>
2018-09-23 15:49:04 +0200

    inital setup

---
+++ src/components/UserCriteriaSelector/Criteria/VoiceChat.tsx
@@ -0,0 +1,44 @@
+import { Form, Checkbox, Divider } from "semantic-ui-react";
+import * as React from "react";
+import { Component } from "react";
+
+interface VoiceChatProps {
+    handleChange: () => void;
+    voicechat: boolean[];
+}
+
+export default class VoiceChat extends Component<VoiceChatProps> {
+    render() {
+        return (
+            <div>
+                <Divider inverted horizontal>Voice chat?</Divider>
+                <Form.Group inline className="centered-form-field">
+                    <Form.Field>
+                        <Checkbox
+                            radio
+                            label="YES"
+                            checked={this.props.voicechat.length === 1 && this.props.voicechat[0] === true}
+                            onChange={this.props.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox
+                            radio
+                            label="NO"
+                            checked={this.props.voicechat.length === 1 && this.props.voicechat[0] === false}
+                            onChange={this.props.handleChange}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox
+                            radio
+                            label="I don't care"
+                            checked={this.props.voicechat.length === 2}
+                            onChange={this.props.handleChange}
+                        />
+                    </Form.Field>
+                </Form.Group>
+            </div>
+        );
+    }
+}
