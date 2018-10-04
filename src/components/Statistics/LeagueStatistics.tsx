be8a69917688a633ab0f43cd80468da30eed0526
MPKohl <martin.pagh.kohl@gmail.com>
2018-10-04 15:55:49 +0200

    add icons for player rank

--- src/components/Statistics/LeagueStatistics.tsx
+++ src/components/Statistics/LeagueStatistics.tsx
@@ -3,41 +3,38 @@ import { Statistic, Image } from "semantic-ui-react";
 
 interface LeagueStatisticProps {
 	iconSrc: string;
-    league: string;
-    division: string;
-    mostPlayed1Src: string;
-    mostPlayed2Src: string;
-    mostPlayed3Src: string;
+	league: string;
+	division: string;
+	mostPlayed1Src: string;
+	mostPlayed2Src: string;
+	mostPlayed3Src: string;
 	positionsSrc: string;
 }
 
 const LeagueStatistics: React.SFC<LeagueStatisticProps> = (props) => {
 	return (
-        <Statistic.Group widths="one" >
-			            
-            <Statistic>
-				<Statistic.Value>
-                        <Image centered circular src={props.iconSrc} />
-                </Statistic.Value>
-				<Statistic.Label>Summoner icon</Statistic.Label>
+		<Statistic.Group widths="one" >
+
+			<Statistic>
+				<Statistic.Label><Image centered circular src={props.iconSrc} /></Statistic.Label>
 			</Statistic>
 
 			<Statistic>
-                <Statistic.Value text>
-                    {props.division}<br />
-                    {props.league}
-                </Statistic.Value>
+				<Statistic.Value text>
+					{props.division}<br />
+					{props.league}
+				</Statistic.Value>
 				<Statistic.Label>League</Statistic.Label>
 			</Statistic>
 
 			<Statistic>
 				<Statistic.Value>
-                    <Image.Group >
-                        <Image centered circular src={props.mostPlayed1Src} />
-                        <Image centered circular src={props.mostPlayed2Src} />
-                        <Image centered circular src={props.mostPlayed3Src} />
-                    </Image.Group>
-                </Statistic.Value>
+					<Image.Group >
+						<Image centered circular src={props.mostPlayed1Src} />
+						<Image centered circular src={props.mostPlayed2Src} />
+						<Image centered circular src={props.mostPlayed3Src} />
+					</Image.Group>
+				</Statistic.Value>
 				<Statistic.Label>Most played champions</Statistic.Label>
 			</Statistic>
 
