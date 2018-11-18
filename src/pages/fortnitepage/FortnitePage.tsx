76d9efb02cb8a65967c8ec1d7159ae4e9ab9ae7c
MPKohl <martin.pagh.kohl@gmail.com>
2018-11-18 17:04:10 +0100

    change logo on fortnitepage to include title

--- src/pages/fortnitepage/FortnitePage.tsx
+++ src/pages/fortnitepage/FortnitePage.tsx
@@ -3,6 +3,7 @@ import "./FortnitePage.css";
 import { RouteComponentProps, Route } from "react-router";
 import FindUserContainer from "../../containers/FindUserContainer";
 import UserPaneContainer from "../../containers/UserPaneContainer";
+import { Image } from "semantic-ui-react";
 
 interface DispatchEvents {
 }
@@ -18,8 +19,7 @@ export default class FortnitePage extends React.Component<AllProps, any> {
 		return (
 			<div className="fortnite-page">
 				<div className="page-content">
-					<p className="buddy" >Buddy<span>GG</span></p>
-					<p>FORTNITE</p>
+					<Image centered style={{ width: 460, height: 230 }} src="/BUDDYGG-LOGO-3-no-text.png" />
 					<FindUserContainer />
 					<Route path={`${this.props.match.url}/:platform/:username`} component={UserPaneContainer} />
 				</div>
