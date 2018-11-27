833c6eb32c3a90faee869dec9fc073178c173c05
Hans <hanspagh@gmail.com>
2018-11-27 22:19:33 +0100

    fix matchingPage design

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,6 +1,67 @@
 import * as React from "react";
 
 import { storiesOf } from "@storybook/react";
+import MatchTable from "../src/components/MatchTable/MatchTable";
+
+const player = {
+  username: "Trolleren",
+  total: {
+    gamesWon: 0,
+    gamesPlayed: 22
+  },
+  squad: {
+    top6finishes: 3,
+    top3finishes: 1,
+    killDeathRatio: 0.6111111111111112,
+    gamesWon: 0,
+    gamesPlayed: 18
+  },
+  solo: {
+    top25finishes: 0,
+    top10finishes: 0,
+    killDeathRatio: 0.375,
+    gamesWon: 0,
+    gamesPlayed: 8
+  },
+  platform: "pc",
+  duo: {
+    top5finishes: 1,
+    top12finishes: 2,
+    killDeathRatio: 0.7142857142857143,
+    gamesWon: 0,
+    gamesPlayed: 14
+  },
+  comment: "asd",
+  name: "Trolleren",
+  languages: [
+    "za"
+  ],
+  ageGroup: "interval1",
+  voiceChat: [
+    true
+  ],
+  id: "20423d24-6351-4bb3-abbe-5e4f0fab05b3",
+  game: "fortnite",
+  criteria: {
+    ageGroups: {
+      interval1: true,
+      interval2: true,
+      interval3: true
+    },
+    voiceChat: {
+      YES: true,
+      NO: true
+    },
+    ignoreLanguage: false
+  },
+  gameInfo: {
+    platform: "pc",
+    gamesPlayed: 1,
+    gameCriteria: {
+      minGamesPlayed: 1
+    }
+  }
+};
 
 storiesOf("Button", module)
-  .add("with text", () => <button>Hello Button</button>);
+  .add("with text", () => <MatchTable matches={[player]} requestMatch={() => true} />);
