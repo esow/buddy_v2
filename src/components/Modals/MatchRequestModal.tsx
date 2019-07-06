ccfee6ab727aa73e7245dc2b0402ce983217097f
lewisbails <lbai@oticon.com>
2019-07-06 14:59:01 +0200

    attempt at modals, didnt do success modal yet, only denied

--- src/components/Modals/MatchRequestModal.tsx
+++ src/components/Modals/MatchRequestModal.tsx
@@ -1,9 +1,10 @@
-import { Modal } from "semantic-ui-react";
-import RequestingPlayerInfo from "./RequestingPlayerInfo";
-import { Button } from "semantic-ui-react";
+import { Modal, Grid } from "semantic-ui-react";
+import { Button, Header, Flag } from "semantic-ui-react";
 import ReactCountdownClock from "react-countdown-clock";
 import * as React from "react";
 import { Component } from "react";
+import "./modal.css";
+import RequestingPlayerInfo from "./RequestingPlayerInfo";
 
 export interface MatchRequestModalProps {
     player: any;
@@ -33,42 +34,44 @@ export default class MatchRequestModal extends Component<MatchRequestModalProps,
     render() {
         const player = this.props.player;
         return (
-            <div>
-                <Modal
-                    closeOnRootNodeClick={false}
-                    dimmer={"blurring"}
-                    className="modal"
-                    open={this.state.open}
-                    onClose={this.props.handleClose}
-                >
-
-                    <Modal.Header>Match found!</Modal.Header>
-                    <Modal.Content className="no-padding">
-                        <RequestingPlayerInfo match={player} />
-                    </Modal.Content>
-                    <Modal.Actions>
-                        <div className="answer-bar">
-                            <div className="button-box">
-                                <Button negative onClick={this.reject}> Cancel </Button>
-                            </div>
-                            <div className="countdown">
-                                <ReactCountdownClock
-                                    seconds={this.props.timeLeft}
-                                    size="60"
-                                    color="black"
-                                    onComplete={this.reject}
-                                    fontColor="white"
-                                    showMilliseconds="false"
-                                />
-                            </div>
-                            <div className="button-box">
-                                <Button positive onClick={this.accept}> Accept </Button>
-                            </div>
-                        </div>
-                    </Modal.Actions>
-                </Modal>
-
-            </div>
+            <Modal
+                closeOnRootNodeClick={false}
+                dimmer={"blurring"}
+                className="modal"
+                open={this.state.open}
+                onClose={this.props.handleClose}
+                size={'small'}
+            >
+                <Grid centered>
+                    <Grid.Row centered><Header>{player.name} has sent you a request!</Header></Grid.Row>
+                    <Grid.Row centered columns={1}>
+                        <Header size="medium">{player.username}</Header>
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        {player.languages.map((element: any) => {
+                            return <Flag name={element} />
+                        })}
+                    </Grid.Row>
+                    <RequestingPlayerInfo player={player} />
+                    <Grid.Row centered>
+                        {player.comment}
+                    </Grid.Row>
+                    <Grid.Row centered className='countdown'>
+                        <ReactCountdownClock
+                            seconds={this.props.timeLeft}
+                            size="60"
+                            color="#334D6E"
+                            onComplete={this.reject}
+                            fontColor="white"
+                            showMilliseconds="false"
+                        />
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        <Button className='cancel-button confirm' onClick={this.accept}> CONFIRM </Button>
+                        <Button className='cancel-button cancel' onClick={this.reject}> CANCEL </Button>
+                    </Grid.Row>
+                </Grid>
+            </Modal>
 
         );
     }
---
+++ stories/modals.stories.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import MatchRequestModal from "../src/components/Modals/MatchRequestModal";
+import MatchResponseModal from "../src/components/Modals/MatchResponseModal";
+import RequestingMatchModal from "../src/components/Modals/RequestingMatchModal";
+
+
+const player = {
+	username: "Trolleren",
+	total: {
+		gamesWon: 0,
+		gamesPlayed: 22
+	},
+	squad: {
+		top6finishes: 3,
+		top3finishes: 1,
+		killDeathRatio: 0.6111111111111112,
+		gamesWon: 0,
+		gamesPlayed: 18
+	},
+	solo: {
+		top25finishes: 0,
+		top10finishes: 0,
+		killDeathRatio: 0.375,
+		gamesWon: 0,
+		gamesPlayed: 8
+	},
+	platform: "pc",
+	duo: {
+		top5finishes: 1,
+		top12finishes: 2,
+		killDeathRatio: 0.7142857142857143,
+		gamesWon: 7,
+		gamesPlayed: 14
+	},
+	comment: "LF decent sniper to carry my sorry ass. No chix plz.",
+	name: "Trolleren",
+	languages: [
+		"za",
+		"dk",
+	],
+	ageGroup: "interval2",
+	voiceChat: [
+		false
+	],
+	id: "20423d24-6351-4bb3-abbe-5e4f0fab05b3",
+	game: "fortnite",
+	criteria: {
+		ageGroups: {
+			age: "22+",
+			interval1: true,
+			interval2: false,
+			interval3: false
+		},
+		voiceChat: {
+			voice: "Yes",
+			YES: true,
+			NO: false
+		},
+		ignoreLanguage: false
+	},
+	gameInfo: {
+		platform: "pc",
+		gamesPlayed: 1,
+		gameCriteria: {
+			minGamesPlayed: 1
+		}
+	}
+};
+
+
+storiesOf("Modals", module)
+	.add("MatchRequestModal with example data", () => (
+		<MatchRequestModal player={player} open={true} timeLeft={200} handleClose={() => { }} handleAccept={() => { }} />)
+	)
+
+	.add("MatchResponseModal with example data", () => (
+		<MatchResponseModal
+			open={true}
+			handleClose={() => { }}
+			response={"Request_Rejected"}
+			player={player}
+		/>)
+	)
+
+	.add("RequestingMatchModal with example data", () => (
+		<RequestingMatchModal
+			open={true}
+			handleClose={() => { }}
+			timeLeft={200}
+			player={player}
+		/>)
+	)
