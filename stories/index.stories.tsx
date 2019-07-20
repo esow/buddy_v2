9e2327c30000637fb2557bc770564ca488d5c857
lewisbails <lbai@oticon.com>
2019-07-20 12:01:47 +0200

    scroll on matches and styling of scrollbar

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -397,3 +397,4 @@ storiesOf("Matching page", module)
       </Grid.Column>
     </Grid.Row>
   </Grid>)
+  .add("with text", () => <MatchingPage matches={[player, player2, player3, player4, player5, player6]} criteria={true} />);
